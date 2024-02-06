86da9718d13d2f2cf93d63a4d3c3a5633b2a411c
saraferrantini <sara.ferrantini@gmail.com>
2024-02-06 21:40:34 +0100

    inserimento href che rimanda alla seconda pagina

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function inizializza() {
 function spuntaCasellina() {
   let prendiCasellina = document.querySelector("#spuntaPromessa");
   prendiCasellina.addEventListener("click", function () {
-    console.log("casellina spuntata");
     // richiamo l'elemento bottone procedi
     let bottoneProcedi = document.querySelector("#bottone-procedi");
     // verifico lo stato del bottone
@@ -34,9 +33,7 @@ function spuntaCasellina() {
 function procediEsame() {
   let bottoneProcedi = document.querySelector("#bottone-procedi");
   bottoneProcedi.addEventListener("click", function () {
-    console.log("bottone procedi cliccato");
+    console.log("bottone procedi cliccato"); //poi rimuoverlo
+    window.location.href = "https://www.google.it/"; // Inserire l'URL della tua pagina successiva
   });
 }
-
-//in js all'interno di un listener come faccio a indirizzare la pagina verso un altro html?
-//0) disabilitare di default il bottone procedi
