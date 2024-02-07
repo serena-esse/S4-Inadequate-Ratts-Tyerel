d51a551dc6c73cae682f2523a167fcde9ec50544
RiccardoAD <rick_95@libero.it>
2024-02-07 16:47:48 +0100

    third.commit
    rifinito il css , aggiunto valutazione da 0 a 10 con consle log e ell'entualita l return

--- pagina4.js
+++ pagina4.js
@@ -1,28 +1,45 @@
+
+let lastUserVote = 0; // Variabile globale per memorizzare l'ultimo voto dell'utente
  function stelle () {
-    
+    const stars = document.querySelectorAll(".star");
+      stars.forEach((star,index)=>{   // index indica l'indice dell'elemnto nella nodelist
+         star.addEventListener("click",function () {  // funzione che si attiva al click su una stella 
+             stars.forEach((starElements,iStar)=>{ // iStar è l'indice dell'interazione 
+                 if (iStar<=index) {          // se l'indice delaa stella iterata è  inferiore o uguale a 
+                     starElements.classList.add("active"); //quella della stella cliccata .rende la stella active 
+                 } else {
+                     starElements.classList.remove("active"); // se è superiore a rimuove , disattiva le stelle sucessive a quella cliccata 
+                 }
+             });
+           lastUserVote=index+1
+             console.log("il voto dell'utente è :",lastUserVote);   //stampo sulla console il valore della stella cliccata
+         });
+
      
-     const stars=document.querySelectorAll(".star");
-     stars.forEach((star,index)=>{   // index indica l'indice dell'elemnto nella nodelist
-        star.addEventListener("click",function () {  // funzione che si attiva al click su una stella 
-            stars.forEach((starElements,iStar)=>{ // iStar è l'indice dell'interazione 
-                if (iStar<=index) {          // se l'indice delaa stella iterata è  inferiore o uguale a 
-                    starElements.classList.add("active"); //quella della stella cliccata .rende la stella active 
-                } else {
-                    starElements.classList.remove("active"); // se è superiore a rimuove , disattiva le stelle sucessive a quella cliccata 
-                }
-            });
-            console.log("il voto dell'utente è :",index+1);  //stampo sulla console il valore della stella cliccata
-        });
-    });
-    
-};
- function bottone() {                                   //funzione che aggiunge l'event al botto che porta ad una pagina web 
-    const button=document.querySelector(".bottone");
-    button.addEventListener("click",function(){
+
         
-        window.location.href="#"                   
-    })
- }
+     });
+
+};
+
+
+  function bottone() {                                   //funzione che aggiunge l'event al botto che porta ad una pagina web 
+     const button=document.querySelector(".bottone");
+     const textarea=document.querySelector(".textarea")
+    button.addEventListener("click",function(){              // stampa il contenuto della textare 
+       const valoreTextarea=textarea.value;
+       console.log("il voto dell'utente è :",lastUserVote);
+ 
+        console.log("contenuto textarea :",valoreTextarea);
+        setTimeout(()=>{                             // metto un ritardo di 2 secpndi( scritto in millisecondi) prima del reinderizzamento
+            window.location.href="#" ;                  
+
+
+        },2000);     
+     
+     })
+
+  }
 
 
 window.onload= function() {
