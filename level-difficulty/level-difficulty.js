a8cb19dfc352dff937e07eaa8337cab6c27cec55
saraferrantini <sara.ferrantini@gmail.com>
2024-02-08 19:50:47 +0100

    modifiche

---
+++ level-difficulty/level-difficulty.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+  inizializza();
+};
+
+function inizializza() {
+  procediEsame();
+}
+
+//BOTTONE
+//1)Prendiamo il bottone dove vogliamo agire
+//2)Ci mettiamo un ascoltatore che riceve l'input sia del livello di difficoltà che del numero di domande
+
+function procediEsame() {
+  let bottoneProcedi = document.querySelector("#bottone-procedi");
+  const levels = ["Low", "Medium", "High"];
+  bottoneProcedi.addEventListener("click", function () {
+    let sliderInput = document.getElementById("difficulty-slider");
+    const level = levels[sliderInput.value - 1];
+    console.log(level);
+    let numeroDomande = document.getElementById("numero-domande");
+    console.log(numeroDomande.value);
+
+    // PER INVIARE I DATI ALLA PAGINA SUCCESSIVA
+    let parametroDomande = encodeURIComponent(numeroDomande.value);
+    let parametroLivello = encodeURIComponent(sliderInput.value);
+
+    // Costruisci l'URL con i parametri
+    //al posto di pagina2 inserire come Marco ha rinominato il suo index.
+    //il ? indica che inizierà la parte dei parametri che noi indichiamo con parametroDomande e parametroLivello
+    let nuovoURL = "pagina2.html?parametroDomande=" + parametroDomande + "&parametroLivello=" + parametroLivello;
+
+    // Reindirizza alla nuova pagina
+    window.location.href = nuovoURL;
+
+    // window.location.href = "https://www.google.it/"; // Inserire l'URL della tua pagina successiva
+  });
+}
+
+// PAGINA3 MARCO DEVE INSERIRE QUESTE TRE RIGHE nel window.onload
+let urlParams = new URLSearchParams(window.location.search);
+let parametroDomande = urlParams.get("parametroDomande");
+let parametroLivello = urlParams.get("parametroLivello");
